f8885318f36fbf891e096b1980824cf6364d095f
Paul Burel <paulburel400@gmail.com>
2023-01-31 15:46:43 +0100

    ajout du serveur eureka

---
+++ my_dashboard/src/main/java/com/howtodoinjava/example/eureka/EurekaServer.java
@@ -0,0 +1,16 @@
+package com.howtodoinjava.example.eureka;
+
+import de.codecentric.boot.admin.config.EnableAdminServer;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+@EnableAdminServer
+public class EurekaServer {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServer.class, args);
+    }
+}
